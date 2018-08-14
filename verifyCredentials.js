12b0f251071fae13907f19dd28ec3891a8ca5bb6
Sebastian Lang <sebastian-lang@telekom.de>
2018-08-14 12:46:58 +0200

    migration from bitbucket

---
+++ verifyCredentials.js
@@ -0,0 +1,17 @@
+﻿"use strict";
+const request = require('request-promise');
+
+module.exports = verify;
+
+/**
+ * Executes the verification logic by sending a simple to the Petstore API using the provided apiKey.
+ * If the request succeeds, we can assume that the apiKey is valid. Otherwise it is not valid.
+ *
+ * @param credentials object to retrieve apiKey from
+ *
+ * @returns Promise sending HTTP request and resolving its response
+ */
+function verify(credentials) {
+    console.log('About to verify credentials');
+    cb(null, { verified: true });
+}
